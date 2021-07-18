4a7b933612f0670fb1692118551fa943107067ee
oi <onno.invernizzi@ivention.nl>
2021-07-18 11:43:17 +0200

    Project setup

---
+++ GameOptimizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameOptimizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
