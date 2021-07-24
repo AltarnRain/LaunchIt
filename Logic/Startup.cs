d1ff2c5bc5f74238f1f2230c056419fbce61cd84
oi <onno.invernizzi@ivention.nl>
2021-07-24 10:43:52 +0200

    Yeah, lets skip on command line parsing for now.

--- Logic/Startup.cs
+++ Logic/Startup.cs
@@ -4,49 +4,17 @@
 
 namespace Logic
 {
-    using Logic.Common;
-    using Logic.Providers;
-
     /// <summary>
     /// Startup class for the program.
     /// </summary>
     public class Startup
     {
         /// <summary>
-        /// The window service.
-        /// </summary>
-        private readonly IRunningProgramsHelper windowServices;
-
-        /// <summary>
-        /// The logger.
-        /// </summary>
-        private readonly ILogger logger;
-
-        private readonly IPathProvider pathProvider;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Startup" /> class.
-        /// </summary>
-        /// <param name="windowServicesProvider">The window services provider.</param>
-        /// <param name="logger">The logger.</param>
-        /// <param name="pathProvider">The path provider.</param>
-        public Startup(IRunningProgramsHelper windowServicesProvider, ILogger logger, IPathProvider pathProvider)
-        {
-            this.windowServices = windowServicesProvider;
-            this.logger = logger;
-            this.pathProvider = pathProvider;
-        }
-
-        /// <summary>
         /// Starts the specified arguments.
         /// </summary>
         /// <param name="args">The arguments.</param>
         public void Start(string[] args)
         {
-            this.logger.Log("I ran");
-            var p = this.pathProvider.MapPath("~/");
-
-            this.logger.Log(p);
         }
     }
 }
