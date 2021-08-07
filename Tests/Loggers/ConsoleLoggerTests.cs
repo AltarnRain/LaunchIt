883ce72e989c0e66e9f1609caa93c3d65a955b53
oi <onno.invernizzi@ivention.nl>
2021-08-07 08:50:40 +0200

    Coverage for ConsoleLogger.

---
+++ Tests/Loggers/ConsoleLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="ConsoleLoggerTests.cs" company="Antonio Invernizzi V">
+// Copyright (c) Antonio Invernizzi V. All rights reserved.
+// </copyright>
+
+namespace Infrastructure.Loggers.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Tests <see cref="ConsoleLogger"/>.
+    /// </summary>
+    [TestClass]
+    public class ConsoleLoggerTests
+    {
+        /// <summary>
+        /// Logs the test.
+        /// </summary>
+        [TestMethod]
+        public void LogTest()
+        {
+            // Arrange
+            var target = new ConsoleLogger();
+
+            // Act
+            target.Log("Hello world");
+        }
+    }
+}
